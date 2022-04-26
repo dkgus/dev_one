7649080666e3a8af0ca39bc7998946403b35d672
dkgus12dkgus <dkgus12dkgus@naver.com>
2022-04-26 16:41:07 +0900

    ADD: server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-111;
+const express = require("express");
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API running"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
